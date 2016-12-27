c0080ade052318df45418b6c3e8c761805347ff9
mohitkd <dmohit@thoughtworks.com>
2016-12-27 14:51:39 +0530

    Update formConditions.js

--- formConditions.js
+++ formConditions.js
@@ -1,8 +1,8 @@
 Bahmni.ConceptSet.FormConditions.rules = {
-    'Diastolic Data' : function (formName, formFieldValues) {
-        var systolic = formFieldValues['Systolic'];
-        var diastolic = formFieldValues['Diastolic'];
-        if (systolic || diastolic) {
+    'Blood Pressure' : function (formName, formFieldValues) {
+        var bloodPressure = formFieldValues['Blood Pressure'];
+        var regex = /\d/g;
+        if (regex.test(bloodPressure)) {
             return {
                 enable: ["Posture"]
             }
@@ -12,17 +12,63 @@ Bahmni.ConceptSet.FormConditions.rules = {
             }
         }
     },
-    'Systolic Data' : function (formName, formFieldValues) {
-        var systolic = formFieldValues['Systolic'];
-        var diastolic = formFieldValues['Diastolic'];
-        if (systolic || diastolic) {
+    'Diastolic Data' : function (formName, formFieldValues) {
+        var diastolic = formFieldValues['Diastolic Data'];
+        if (diastolic) {
             return {
                 enable: ["Posture"]
             }
         } else {
-            return {
-                disable: ["Posture"]
-            }
+            return {}
         }
+    },
+
+'Swasthya Employee' : function (formName, formFieldValues) {
+        var employee = formFieldValues['Swasthya Employee'];
+        if (employee == "No") {
+            return {
+	enable: ["REGISTRATION FEES"]}
+            }
+        else {
+            return {
+	disable: ["REGISTRATION FEES"]
+}
+        }
+    },
+
+'Returning Patient With ID' : function (formName, formFieldValues) {
+        var retpatient = formFieldValues['Returning Patient With ID'];
+        if (retpatient == "No") {
+            return {
+        enable: ["FINE"]}
+            }
+        else {
+            return {
+        disable: ["FINE"]
+}
+        }
+    },
+
+'From Tribal Village' : function (formName, formFieldValues) {
+        var tribal = formFieldValues['From Tribal Village'];
+        if (tribal == "No") {
+            return {
+        enable: ["REGISTRATION FEES"]}
+            }
+        else {
+            return {
+        disable: ["REGISTRATION FEES"]
+}
+        }
+    },
+
+
+'Systolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic Data'];
+        if (systolic) {
+            return {
+                enable: ["Posture"]
+            }
+        } else {}
     }
 };
